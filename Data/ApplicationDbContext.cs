84809484bc5e71b96d72bd8e8eb4115ac9a8b58c
zielinsky <mrzbiznes@gmail.com>
2022-09-21 23:17:19 +0200

    Add EntityFramework with Sqlite

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mysos_web_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using mysos_web_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(@"C:\Users\MrZ13\source\repos\mysos_web_api\Data\Database.db");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
